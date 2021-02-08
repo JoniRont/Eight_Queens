3128031377923821eb8fb98677b085276b243fd6
JoniRont <57128169+JoniRont@users.noreply.github.com>
2021-02-09 00:08:25 +0200

    paranneltu koodia ja lisätty readme tiedostoon sisältöä

--- Chessboard/src/main/java/Chessboard/EightQueens.java
+++ Chessboard/src/main/java/Chessboard/EightQueens.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-/**
- * Paranneltu bruteforce menetelmä etsimään kaikki vaihtoehdot asettaa
- * kuningattaret pelilaudalle uhkaamatta toisiaan pelilaudan kokoa voi
+/** HUOM!!! värit eivät toimi kaikissa terminaaleissa (powershell/windows komentokehoite), vscode terminaali sen sijaan toimii,
+ * mikäli tämä häiritsee tulee kommentoida/poistaa printer() metodista kaikki ANSI_<color> rivit.
+ * 
+ * Paranneltu bruteforce/backtracking menetelmä etsimään kaikki vaihtoehdot asettaa
+ * kuningattaret pelilaudalle uhkaamatta toisiaan: Pelilaudan kokoa voi
  * muuttaa(minimi 4), maksimi ei ole mutta luonnollisesti mitä isompi pelilauta
  * == tarvitaan tehoja koneesta oletuksena on kahdeksan jolloin pitäisi löytyä
  * 92 eri vaihtoehtoa
@@ -28,8 +30,6 @@ import java.util.Scanner;
  * 12 x 12 14,200 
  * 13 x 13 73,712 
  * 14 x 14 365,596
- *
- * todo: ohjelmassa saattaa olla ylimääräisiä ominaisuuksia sekä ehtolauseita
  */
 public class EightQueens {
   public static final String ANSI_YELLOW = "\u001B[33m";
@@ -38,20 +38,21 @@ public class EightQueens {
     Scanner input = new Scanner(System.in);
     // Pelikentän koon alustaminen! sekä eri tulostuksien määrän asettaminen
     // -------------------------------------------------------------
-    
     int chessBoardSize=4;
     int printValue=1;
     try {
       System.out.print("Anna pelilaudan koko: ");
-      chessBoardSize = input.nextInt();
+      chessBoardSize = Integer.parseInt(input.nextLine());
       System.out.print("Anna tulostuksien määrä: ");
-      printValue = input.nextInt();
+      printValue = Integer.parseInt(input.nextLine());
+      if(printValue>chessBoardSize)printValue=chessBoardSize;
       input.close();
     } catch (Exception e) {
       System.out.println("Virhe: "+e);
       return;
     }
     // -------------------------------------------------------------
+
     QSolver s = new QSolver(chessBoardSize);
     System.out.println("Ohjelma alkoi ja etsitään " +ANSI_YELLOW +chessBoardSize +ANSI_RESET+ " kuningattarelle paikat.");
     long start = System.currentTimeMillis();
@@ -65,7 +66,7 @@ public class EightQueens {
 }
 
 class QSolver {
-  public static final String ANSI_RESET = "\u001B[0m";
+  public static final String ANSI_RESET = "\033[0m";
   public static final String ANSI_BLACK = "\u001B[30m";
   public static final String ANSI_RED = "\u001B[31m";
   public static final String ANSI_GREEN = "\u001B[32m";
@@ -84,29 +85,24 @@ class QSolver {
   public void printer(ArrayList<int[][]> arr, int boardWidth , int howManyToPrint) {
     int counter=howManyToPrint;
     for (int[][] a : arr) {
-      
       if(counter<=0)break;
       System.out.println("Tulostus numero: "+(howManyToPrint-counter+1));
       for (int i = 0; i < a.length; i++) { //this equals to the row in our matrix.
         for (int j = 0; j < a[i].length; j++) {
           if(a[i][j]==8){
-            System.out.print(ANSI_GREEN+"X"+ANSI_RESET+" ");
+            //System.out.print(ANSI_GREEN+"X");         <---- Värit eivät toimi kaikissa terminaaleissa
+            System.out.print((j+1)+" ");
           } 
           else{
             System.out.print("* ");
-
+            //System.out.print(ANSI_RESET+"* ");         <---- Värit eivät toimi kaikissa terminaaleissa
           }
         }
-        System.out.println(); //change line on console as row comes to end in the matrix.
+        System.out.println();
      }
      System.out.println("-----------------------");
      counter-=1;
-      
     }
-    
-    
-    
-    
   }
   public int getsolutionsCount(){
     return this.solutionsCount;
@@ -122,77 +118,62 @@ class QSolver {
                                                             // sijoituksista
     ArrayList<ArrayList<Integer>> allPathsInList = new ArrayList<ArrayList<Integer>>();
     int col = 0;
-    int counter = 0;
     int startRow = 0;
     int addedQuuens = 0;
     int flawColumn = 0;
-    int lastGoodPoint = -1;
-
+    
     while (true) {
-      if (col == 0 && startRow >= this.size - 1)
+      if (col == 0 && startRow > this.size - 1)
         break; // mikäli kaikki vaihtoehdot ovat käyty läpi pysähtyy ohjelma tähän
       flawColumn = 0;
-      counter = 0;
+      
       for (int row = startRow; row < this.size; row++) {
-        if (counter >= this.size)
-          break;
-        if (isPossible(col, row, board) && row != lastGoodPoint && !tempPath.contains(row)) {
+        
+        if (isPossible(col, row, board,tempPath)) {
           addedQuuens += 1;
           board[col][row] = 8;
           tempPath.add(row);
           col += 1;
-          lastGoodPoint = row;
           startRow = 0;
           break;
+        }else{
+          flawColumn += 1;
+          board[col][row] = 0;
         }
-        counter += 1;
-        flawColumn += 1;
-        board[col][row] = 0; // testissä oli 1 jotta pysty seuraa paikoitusta debuggauksessa
       }
+      
       if (flawColumn >= this.size || (flawColumn + startRow) >= this.size || col >= this.size) {
         if (addedQuuens == this.size) {
-          ArrayList<Integer> tempArrayPath = new ArrayList<Integer>(tempPath);
-          int[][] destination = Arrays.stream(board).map(a -> Arrays.copyOf(a, a.length)).toArray(int[][]::new);
-          allPaths.add(destination);
-          allPathsInList.add(tempArrayPath);
+          allPaths.add(Arrays.stream(board).map(a -> Arrays.copyOf(a, a.length)).toArray(int[][]::new)); //kopioidaan oikea ratkaisu array allPath listaan
+          allPathsInList.add(new ArrayList<Integer>(tempPath));
         }
-        counter = 0;
         addedQuuens -= 1;
-        lastGoodPoint = tempPath.get(tempPath.size() - 1);
-        startRow = lastGoodPoint;
+        startRow = tempPath.get(tempPath.size() - 1)+1;
         col = col - 1;
-        for (int i = col; i < board.length; i++) {
-          for (int j = 0; j < board.length; j++) {
-            board[i][j] = 0;
-          }
-        }
+        board=clearBoard(board,col);
         tempPath.remove(tempPath.size() - 1);
       }
-
     }
     this.solutionsCount=allPathsInList.size();
     return allPaths;
   }
 
-  private boolean isPossible(int posCol, int posRow, int[][] queensOnBoard) {
+  private boolean isPossible(int posCol, int posRow, int[][] queensOnBoard, ArrayList<Integer> checkAllQueenPositions) {
     /**
      * Metodi tarkistaa voiko kyseiseen ruutuun sijoittaa kuningattaren mikäli voi
      * palauttaa true arvon, muuten palauttaa falsen.
      */
-    int tempRow = posRow;
-    // tarkistetaan taakse
-    for (int i = posCol; i >= 0; i--) {
-      if (queensOnBoard[i][posRow] == 8) {
-        return false;
-      }
+    // Tarkistetaan kaikki rivit taaksepäin onko rivi vapaa
+    if(checkAllQueenPositions.contains(posRow)){
+      return false;
     }
-
+    int tempRow = posRow;
+    
     // tarkistetaan vasen yläviisto
     for (int i = posCol; i >= 0; i--) {
       if (tempRow < 0) {
         break;
       }
-
       if (queensOnBoard[i][tempRow] == 8) {
         return false;
       }
@@ -211,6 +192,18 @@ class QSolver {
       tempRow += 1;
     }
     return true;
+  }
+
+  private int[][] clearBoard(int[][] boardToClean,int toColumn){
+    /**
+     * Alustetaan pelilauta nollilla toColumniin asti jotta voidaan jatkaa puhtaalta pöydältä
+     */
+    for (int i = toColumn; i < boardToClean.length; i++) {
+      for (int j = 0; j < boardToClean.length; j++) {
+        boardToClean[i][j] = 0;
+      }
+    }
+    return boardToClean;
   }
 
 }
--- README.md
+++ README.md
@@ -1,8 +1,38 @@
 # Eight_Queens
 
-Eräänlainen ratkaisu seuraavaan ongelmaan:
+Eräänlainen Backtracking-ratkaisu seuraavaan ongelmaan:
 Kahdeksan kuningattaren ongelma on klassinen kombinatoriikan ongelma, 
 jossa tehtävänä on asettaa kahdeksan kuningatarta shakkilaudalle siten, 
 etteivät mitkään kaksi kuningatarta uhkaa toisiaan; toisin sanoen mitkään 
 kaksi kuningatarta eivät saa olla samalla vaakarivillä, pystylinjalla tai diagonaalilla.
+
+ * HUOM!!! värit eivät toimi kaikissa terminaaleissa (powershell/windows komentokehoite), vscode terminaali sen sijaan toimii,
+ * mikäli tämä häiritsee tulee kommentoida/poistaa printer() metodista kaikki ANSI_<color> rivit.
+ * 
+ * Paranneltu bruteforce/backtracking menetelmä etsimään kaikki vaihtoehdot asettaa
+ * kuningattaret pelilaudalle uhkaamatta toisiaan: Pelilaudan kokoa voi
+ * muuttaa(minimi 4), maksimi ei ole mutta luonnollisesti mitä isompi pelilauta
+ * == tarvitaan tehoja koneesta oletuksena on kahdeksan jolloin pitäisi löytyä
+ * 92 eri vaihtoehtoa
+ * 
+ * https://www.datagenetics.com/blog/august42012/ Sivulta voi tarkistaa toimiiko
+ * algoritmi sekä löytyy kaikki eri vaihtoehdot pelilaudan ratkaisuiden määrä
+ * koko 
+ * 1 x 1 1 
+ * 2 x 2 0 
+ * 3 x 3 0 
+ * 4 x 4 2 
+ * 5 x 5 10 
+ * 6 x 6 4 
+ * 7 x 7 40 
+ * 8 x 8 92 
+ * 9 x 9 352 
+ * 10 x 10 724 
+ * 11 x 11 2,680 
+ * 12 x 12 14,200 
+ * 13 x 13 73,712 
+ * 14 x 14 365,596
+ 
+
+
 
