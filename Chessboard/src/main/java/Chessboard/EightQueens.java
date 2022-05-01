ab813ae1ede76282e668aec4b15de8d86a117b61
Joni Röntynen <57128169+JoniRont@users.noreply.github.com>
2022-05-02 00:51:57 +0300

    Updated main comments

--- Chessboard/src/main/java/Chessboard/EightQueens.java
+++ Chessboard/src/main/java/Chessboard/EightQueens.java
@@ -4,98 +4,94 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-/** HUOM!!! värit eivät toimi kaikissa terminaaleissa (powershell/windows komentokehoite), vscode terminaali sen sijaan toimii,
- * mikäli tämä häiritsee tulee kommentoida/poistaa printer() metodista kaikki ANSI_<color> rivit.
+/** 
  * 
- * Paranneltu bruteforce/backtracking menetelmä etsimään kaikki vaihtoehdot asettaa
+ * Backtracking menetelmä etsimään kaikki vaihtoehdot asettaa
  * kuningattaret pelilaudalle uhkaamatta toisiaan: Pelilaudan kokoa voi
  * muuttaa(minimi 4), maksimi ei ole mutta luonnollisesti mitä isompi pelilauta
- * == tarvitaan tehoja koneesta oletuksena on kahdeksan jolloin pitäisi löytyä
+ * == tarvitaan tehoja koneesta. oletuksena on kahdeksan jolloin pitäisi löytyä
  * 92 eri vaihtoehtoa
  * 
  * https://www.datagenetics.com/blog/august42012/ Sivulta voi tarkistaa toimiiko
  * algoritmi sekä löytyy kaikki eri vaihtoehdot pelilaudan ratkaisuiden määrä
- * koko 
- * 1 x 1 1 
- * 2 x 2 0 
- * 3 x 3 0 
- * 4 x 4 2 
- * 5 x 5 10 
- * 6 x 6 4 
- * 7 x 7 40 
- * 8 x 8 92 
- * 9 x 9 352 
- * 10 x 10 724 
- * 11 x 11 2,680 
- * 12 x 12 14,200 
- * 13 x 13 73,712 
- * 14 x 14 365,596
+ * pelilaudan koko - ratkaisuja 
+ * 1 x 1        ---     1 
+ * 2 x 2        ---     0 
+ * 3 x 3        ---     0 
+ * 4 x 4        ---     2 
+ * 5 x 5        ---     10 
+ * 6 x 6        ---     4 
+ * 7 x 7        ---     40 
+ * 8 x 8        ---     92 
+ * 9 x 9        ---     352 
+ * 10 x 10      ---     724 
+ * 11 x 11      ---     2,680 
+ * 12 x 12      ---     14,200 
+ * 13 x 13      ---     73,712 
+ * 14 x 14      ---     365,596
  */
 public class EightQueens {
-  public static final String ANSI_YELLOW = "\u001B[33m";
-  public static final String ANSI_RESET = "\u001B[0m";
+  
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    // Pelikentän koon alustaminen! sekä eri tulostuksien määrän asettaminen
-    // -------------------------------------------------------------
+    // Pelikentän koon alustaminen sekä eri tulostuksien määrän asettaminen tapahtuu ohjelman suorittamalla konsolissa asetettavilla määrillä.
+
+    // ------------------------------------------------------------- ALUSTUS KONSOLISSA!!!
     int chessBoardSize=4;
     int printValue=1;
     try {
-      System.out.print("Anna pelilaudan koko: ");
+      System.out.print("Anna pelilaudan koko(kokonaislukuna): ");
       chessBoardSize = Integer.parseInt(input.nextLine());
-      System.out.print("Anna tulostuksien määrä: ");
+      System.out.print("Anna tulostuksien maara(kokonaislukuna): ");
       printValue = Integer.parseInt(input.nextLine());
       if(printValue>chessBoardSize)printValue=chessBoardSize;
       input.close();
     } catch (Exception e) {
-      System.out.println("Virhe: "+e);
+      System.out.println("Virhe, vaara syote: "+e);
+      System.out.println("Ohjelma lopetataan!");
       return;
     }
-    // -------------------------------------------------------------
+    // ------------------------------------------------------------- ALUSTUS KONSOLISSA!!!
 
+    // ilmoitetaan käyttäjälle infot: mitä tehdään -> kauanko aikaa menee -> mitä löytyi -> sekä tulostetaan ne konsoliin
     QSolver s = new QSolver(chessBoardSize);
-    System.out.println("Ohjelma alkoi ja etsitään " +ANSI_YELLOW +chessBoardSize +ANSI_RESET+ " kuningattarelle paikat.");
+    System.out.println("Ohjelma alkoi ja etsitaan " +chessBoardSize + " kuningattarelle paikat.");
     long start = System.currentTimeMillis();
     ArrayList<int[][]> result = new ArrayList<int[][]>(s.startGame());
     long elapsedTimeMillis = System.currentTimeMillis() - start;
     float elapsedTimeSec = elapsedTimeMillis / 1000F;
-    System.out.println("Ratkaisuja löytyi yhteensä: " +ANSI_YELLOW +s.getsolutionsCount() +ANSI_RESET +" kpl.");
-    System.out.println("Aikaa meni: " + ANSI_YELLOW+(elapsedTimeSec) +ANSI_RESET+ " Sekuntia");
+    System.out.println("Ratkaisuja loytyi yhteensa: " +result.size() + " kpl.");
+    System.out.println("Aikaa meni: " + (elapsedTimeSec) + " Sekuntia");
     s.printer(result,chessBoardSize, printValue);
   }
 }
 
 class QSolver {
-  public static final String ANSI_RESET = "\033[0m";
-  public static final String ANSI_BLACK = "\u001B[30m";
-  public static final String ANSI_RED = "\u001B[31m";
-  public static final String ANSI_GREEN = "\u001B[32m";
-  public static final String ANSI_YELLOW = "\u001B[33m";
-  public static final String ANSI_BLUE = "\u001B[34m";
-  public static final String ANSI_PURPLE = "\u001B[35m";
-  public static final String ANSI_CYAN = "\u001B[36m";
-  public static final String ANSI_WHITE = "\u001B[37m";
+  
   private int size;
-  private int solutionsCount;
-  // konstruktori6
+
+  // konstruktori
   public QSolver(int boardSize) {
     size = boardSize;
   }
 
   public void printer(ArrayList<int[][]> arr, int boardWidth , int howManyToPrint) {
+    /**
+     * Tulostaa kaikki listan vaihtoehdot oikean kokoisena(boardWidth) matrsiisina.
+     * Tulostuksia tulostetaan haluttu määrä(howManyToPrint) asti.
+     * kuningattaret on numeroitu asetus järjestyksessä tarkastuksen helpottamiseksi
+     */
     int counter=howManyToPrint;
     for (int[][] a : arr) {
       if(counter<=0)break;
       System.out.println("Tulostus numero: "+(howManyToPrint-counter+1));
-      for (int i = 0; i < a.length; i++) { //this equals to the row in our matrix.
+      for (int i = 0; i < a.length; i++) {
         for (int j = 0; j < a[i].length; j++) {
           if(a[i][j]==8){
-            //System.out.print(ANSI_GREEN+"X");         <---- Värit eivät toimi kaikissa terminaaleissa
             System.out.print((j+1)+" ");
           } 
           else{
             System.out.print("* ");
-            //System.out.print(ANSI_RESET+"* ");         <---- Värit eivät toimi kaikissa terminaaleissa
           }
         }
         System.out.println();
@@ -104,31 +100,27 @@ class QSolver {
      counter-=1;
     }
   }
-  public int getsolutionsCount(){
-    return this.solutionsCount;
-  }
+  
   public ArrayList<int[][]> startGame() {
     /**
      * Varsinainen ohjelma suoritetaan täällä(tällä hetkellä kovinkin sekalainen)
      * tarkoitus on siistiä sitä jakamalla toimintoja eri metodeille
      */
-    ArrayList<Integer> tempPath = new ArrayList<Integer>();
+    ArrayList<Integer> tempPath = new ArrayList<Integer>(); //<- väliaikaista listaa käytetään kuningattarien sijantien tarkistukseen sekä uusien sen hetkisten sijaintien asettamiseen
     int[][] board = new int[this.size][this.size];
-    ArrayList<int[][]> allPaths = new ArrayList<int[][]>(); // tarkempi lista pelilaudasta ja kuningattarien
-                                                            // sijoituksista
-    ArrayList<ArrayList<Integer>> allPathsInList = new ArrayList<ArrayList<Integer>>();
-    int col = 0;
-    int startRow = 0;
-    int addedQuuens = 0;
-    int flawColumn = 0;
+    ArrayList<int[][]> allPaths = new ArrayList<int[][]>(); // tarkempi lista pelilaudasta ja kuningattarien sijoituksista
+    int col = 0; //<-osoittaa sen hetkisen sijainti sarakkeen
+    int startRow = 0; //<- osoittaa sen hetkisen aloitus rivin
+    int addedQuuens = 0; //<- kertoo sen hetkisen kuningattarien määrän pelilaudalla(board)
+    int flawColumn = 0; //<- kertoo rivien määrän jotka tarkistettu, mutta ei pystytty asettamaan kuningatarta
     
     while (true) {
-      if (col == 0 && startRow > this.size - 1)
-        break; // mikäli kaikki vaihtoehdot ovat käyty läpi pysähtyy ohjelma tähän
+      if (col == 0 && startRow > this.size - 1)break;// mikäli kaikki vaihtoehdot ovat käyty läpi pysähtyy ohjelma tähän 
       flawColumn = 0;
-      
+      // Loopataan startRow(vaihtelee riippuen backtracking tilanteesta) sijainnista aina pelilaudan kokoon asti kaikki mahdolliset vaihtoehdot
+      // ja asetetaan kuningatar pelilaudalle sekä päivitetään muuttujat(sijainnit ja kuningattarien määrä), mikäli tämä ei onnistu
+      // asetetaan pelilaudalle(board) 0 ja lisätään hylätyn rivin määrää.
       for (int row = startRow; row < this.size; row++) {
-        
         if (isPossible(col, row, board,tempPath)) {
           addedQuuens += 1;
           board[col][row] = 8;
@@ -138,23 +130,24 @@ class QSolver {
           break;
         }else{
           flawColumn += 1;
-          board[col][row] = 0;
+          board[col][row] = 0;// tyhjä asetetut rivit voi laittaa haluamalla int numerolla debuggauksen helpottamiseksi.
         }
       }
-      
-      if (flawColumn >= this.size || (flawColumn + startRow) >= this.size || col >= this.size) {
+
+      // Mikäli ollaan pelilaudan jollain reunalla tarkistetaan onko kaikki kuningattaret asetettu ja jos on, lisätään ne listaan.
+      // tämän jälkeen "peruutetaan" eli alustetaan muuttujat siten että jatketaan saman kolumnin seuraavalta riviltä ja etsitään 
+      // mahdollista uutta sijoituspaikkaa kuningattarelle
+      if ((flawColumn + startRow) >= this.size || col >= this.size) {
         if (addedQuuens == this.size) {
           allPaths.add(Arrays.stream(board).map(a -> Arrays.copyOf(a, a.length)).toArray(int[][]::new)); //kopioidaan oikea ratkaisu array allPath listaan
-          allPathsInList.add(new ArrayList<Integer>(tempPath));
         }
         addedQuuens -= 1;
         startRow = tempPath.get(tempPath.size() - 1)+1;
         col = col - 1;
-        board=clearBoard(board,col);
+        board=clearBoard(board,col); // metodi joka tyhjentää pelilaudan backtracking vaiheessa haluttuun kolumniin asti
         tempPath.remove(tempPath.size() - 1);
       }
     }
-    this.solutionsCount=allPathsInList.size();
     return allPaths;
   }
 
@@ -163,13 +156,17 @@ class QSolver {
      * Metodi tarkistaa voiko kyseiseen ruutuun sijoittaa kuningattaren mikäli voi
      * palauttaa true arvon, muuten palauttaa falsen.
      */
+
+    //alustetaan väliaikainen muuttuja jota käytetään forloopeissa.
+    int tempRow = posRow;
+
     // Tarkistetaan kaikki rivit taaksepäin onko rivi vapaa
     if(checkAllQueenPositions.contains(posRow)){
       return false;
     }
-    int tempRow = posRow;
     
-    // tarkistetaan vasen yläviisto
+    // tarkistetaan vasen yläviisto kunnes pelilaudan jokin reuna tulee vastaan, jolloin lopetetaan tarkastus
+    // mikäli vastaan tulee kuningatar(8) ilmoitetaan sijainti varatuksi
     for (int i = posCol; i >= 0; i--) {
       if (tempRow < 0) {
         break;
@@ -181,7 +178,8 @@ class QSolver {
     }
 
     tempRow = posRow;
-    // tarkistetaan vasen alaviisto
+    // tarkistetaan vasen alaviisto kunnes pelilaudan jokin reuna tulee vastaan, jolloin lopetetaan tarkastus
+    // mikäli vastaan tulee kuningatar(8) ilmoitetaan sijainti varatuksi
     for (int i = posCol; i >= 0; i--) {
       if (tempRow >= queensOnBoard.length) {
         break;
@@ -196,7 +194,7 @@ class QSolver {
 
   private int[][] clearBoard(int[][] boardToClean,int toColumn){
     /**
-     * Alustetaan pelilauta nollilla toColumniin asti jotta voidaan jatkaa puhtaalta pöydältä
+     * Alustetaan pelilauta nollilla mikäli haluttuun kohtaan(toColumn) ollaan menossa taaksepäin backtracking askelilla
      */
     for (int i = toColumn; i < boardToClean.length; i++) {
       for (int j = 0; j < boardToClean.length; j++) {
